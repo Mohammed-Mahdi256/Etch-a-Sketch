9b61fe43114bcdae5fcc2ebff27cc8046e411f0e
Mohammed-Mahdi256 <mohammedmahdi1402@gmail.com>
2023-08-12 16:10:41 +0200

    Added a button to change the dimension of the grid the grid should be between 1 and 100

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
-const container = document.querySelector('.grid-container');
+const container = document.querySelector('.container');
 
-// create 16 row flexbox containing 16 divs
-// then put the rows in one column flexbox in the container
+const changeDimBtn = document.querySelector('.btnChangeDim');
+
+
+function changeDim(message){
+    let newDim = Math.floor(Number(prompt(message)));
+    if(typeof newDim === 'number' && newDim > 0 && newDim <= 100){
+        eraseCurrentGrid();
+        createGrid(newDim);
+    }
+    else{changeDim(message='Invalide input! try again, the dimension should be a positive integer less than 100:')}
+ 
+
+
+}
+
+function eraseCurrentGrid(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+}
+
+
+changeDimBtn.addEventListener('click', () => changeDim('enter the dimension (100 max):'));
+
 
 function createGrid(dimension){
     if(typeof dimension !== 'number' || dimension <= 0 || dimension >100) return 'ERROR';
+
     for (let i=0; i<dimension; i++) {  // create 16 rows
         let row = document.createElement('div');
         row.classList.add('row');
-
+        
         // fill every row with 16 divs
         for(let j=0; j<dimension; j++){
             let div = document.createElement('div');
             div.classList.add('unit');
+            
+            //determine the size depending on the dimension and the total gaps between the boxes
+            totalGap = (dimension - 1) * 2; //there are dimension - 1 gaps each is 2px
+            unitSize = (300 - totalGap) / dimension;
+            div.style.width = `${unitSize}px`;
+            div.style.height = `${unitSize}px`;
 
             addHover(div);
 
@@ -35,8 +64,5 @@ function addHover(div){
     div.addEventListener('mouseenter', () => {
     div.classList.add('hovering')
     })
-
-    div.addEventListener('mouseleave', () => {
-        div.classList.remove('hovering');
-    })
 }
+
