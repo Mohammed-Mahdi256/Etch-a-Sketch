04b6bd8c5873e8be940a3cadd24d61e2e9017270
Mohammed-Mahdi256 <mohammedmahdi1402@gmail.com>
2023-08-12 13:30:29 +0200

    Fixed typo, filter to the dimension only positive

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const container = document.querySelector('.grid-container');
 // create 16 row flexbox containing 16 divs
 // then put the rows in one column flexbox in the container
 
-function createGrid(dimention){
-    if(!typeof dimention === 'number' && !dimention<=100) return 'ERROR';
-    for (let i=0; i<dimention; i++) {  // create 16 rows
+function createGrid(dimension){
+    if(typeof dimension !== 'number' || dimension <= 0 || dimension >100) return 'ERROR';
+    for (let i=0; i<dimension; i++) {  // create 16 rows
         let row = document.createElement('div');
         row.classList.add('row');
 
         // fill every row with 16 divs
-        for(let j=0; j<dimention; j++){
+        for(let j=0; j<dimension; j++){
             let div = document.createElement('div');
             div.classList.add('unit');
+
+            addHover(div);
+
             row.appendChild(div);
         }
 
@@ -25,3 +28,15 @@ function createGrid(dimention){
 
 createGrid(16);
 createGrid('pizza');
+
+
+function addHover(div){
+    // pass a div and make it change color as the mouse hovers over it
+    div.addEventListener('mouseenter', () => {
+    div.classList.add('hovering')
+    })
+
+    div.addEventListener('mouseleave', () => {
+        div.classList.remove('hovering');
+    })
+}
